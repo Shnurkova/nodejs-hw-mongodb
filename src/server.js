8b8d9296c3514b28f13e18723b486194f7073e68
Shnurkova <Annshnur@gmail.com>
2024-07-15 22:54:55 +0300

    add

--- src/server.js
+++ src/server.js
@@ -1,9 +1,30 @@
 import express from 'express';
-import pino from 'pino';
+import pino from 'pino-http';
 import cors from 'cors';
 
 function setupServer() {
     const app = express();
+
+    app.use(pino());
+    app.use(cors());
+
+    app.get("/", (req, res) => {
+        res.send([
+            {
+                id: 1,
+                title: 'Title 1',
+            },
+            {
+                id: 2,
+                title: 'Title 2',
+            },
+            {
+                id: 3,
+                title: 'Title 3',
+            },
+        ]);
+    });
+
     app.listen(3000, () => {
         console.log("Server is running on port 3000");
     });
