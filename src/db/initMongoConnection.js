e140614f9a59028c1b4e5dbb34631894951e8c18
Shnurkova <Annshnur@gmail.com>
2024-07-17 20:17:26 +0300

    add

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import mongoose from "mongoose";
+
+dotenv.config();
 
 async function initMongoConnection() {
     try {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import setupServer from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
-import dotenv from 'dotenv';
-
-
-dotenv.config();
 
 async function bootstrap() {
     try {
