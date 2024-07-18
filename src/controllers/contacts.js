1f4bec1a0891cfd4d75a0dc280d925e8f5021fad
Shnurkova <Annshnur@gmail.com>
2024-07-18 21:33:23 +0300

    add

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,1 +1,15 @@
-export const getAllContacts = async (req, res, next) => { };
+import { getAllContactsServices } from '../services/contacts.js';
+
+export const getAllContacts = async (req, res, next) => {
+    try {
+        const contacts = await getAllContactsServices;
+        res.status(200).json({
+            message: "Successfully found contact with id {**contactId**}!",
+            data: contacts,
+            status: 200,
+        });
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
