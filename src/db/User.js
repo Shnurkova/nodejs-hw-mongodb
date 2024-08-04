0d81d3a735d5d2d32311641f56552bc0fada17f6
Shnurkova <Annshnur@gmail.com>
2024-08-04 18:01:54 +0300

    add schema

---
+++ src/db/User.js
@@ -0,0 +1,21 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model('user', userSchema);
+
+export default User;
