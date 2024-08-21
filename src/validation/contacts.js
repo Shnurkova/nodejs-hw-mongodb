fb4a83c5b0354d7cba390b09e61daefc137f18ac
Shnurkova <Annshnur@gmail.com>
2024-08-21 19:09:50 +0300

    fix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,6 +6,7 @@ export const contactSchema = Joi.object({
   email: Joi.string().min(3).max(20).email().optional(),
   isFavourite: Joi.boolean().optional(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  photo: Joi.string().optional(),
 });
 
 export const updateContactSchema = Joi.object({
